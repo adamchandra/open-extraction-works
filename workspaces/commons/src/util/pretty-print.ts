8a38fa4aedd8364db058be2a0f699cecea31d481
Adam Saunders <saunders@cs.umass.edu>
2020-07-17 11:43:45 -0400

    prettyPrint/putLn mods

--- workspaces/commons/src/util/pretty-print.ts
+++ workspaces/commons/src/util/pretty-print.ts
@@ -10,7 +10,7 @@ function getCallerContext() {
   // Get caller context
   const err = getErrorObject();
   const stack = err.stack;
-  let lines = stack? stack.split('\n') : [];
+  let lines = stack ? stack.split('\n') : [];
   lines = _.dropWhile(lines, (l) => !l.includes('at prettyPrint'));
   lines = _.drop(lines, 1);
   lines = _.takeWhile(lines, l => !l.includes('node_modules'));
@@ -106,8 +106,8 @@ const inspectOptionDefaults = {
 
 
 /**
- * Prints out one or more expressions via console.log, with a few improvements
- *   to make debug printing easier.
+ * Prints out one or more expressions directly to process.stdout, with a few
+ *   improvements over console.log to make debug printing easier.
  * Usage:
  *   let x=1, y=2, z=3;
  *   prettyPrint({x, y, z}); <== note use of braces
@@ -122,7 +122,9 @@ const inspectOptionDefaults = {
  *      z: 3
  *      ===
  */
-export function prettyPrint(vsobj: object, options: Partial<InspectOptions> = {}): void {
+const [fst, lst, mid, sole] = "╭╰│═".split(''); /*  "┏┗┃═" */
+
+export function prettyPrint(vsobj: any, options: Partial<InspectOptions> = {}): void {
   let callerContext = '';
   const opts = Object.assign({}, inspectOptionDefaults, options);
 
@@ -132,15 +134,44 @@ export function prettyPrint(vsobj: object, options: Partial<InspectOptions> = {}
   }
 
   const props = Object.getOwnPropertyNames(vsobj);
+  const lens = _.map(props, p => p.length)
+  const maxlen = _.max(lens) || 0;
 
-  const fmt = _.join(_.map(props, p => {
+  const fmt = _.join(_.map(props, (p, pi) => {
+    const isSolo = props.length === 1;
+    const isFirst = pi === 0;
+    const isLast = pi === props.length - 1;
+    const prefixChar = isSolo ? sole
+      : isFirst ? fst
+        : isLast ? lst
+          : mid;
     const o = vsobj as any;
     const v = o[p];
     const ins = util.inspect(v, opts);
-    return p + ': ' + ins;
+    const insLines = ins.split('\n');
+    const ins0 = insLines[0];
+
+    const indented = _.map(insLines.slice(1), (l, li) => {
+      const indentPad = "".padEnd(maxlen);
+      return `    ${indentPad}${l}`;
+    }).join('\n');
+    const continuation = indented.length > 0? `\n${indented}` : ''
+    return `  ${prefixChar} ${p.padEnd(maxlen)}: ${ins0}${continuation}`;
   }), '\n');
 
   const output = callerContext + fmt;
-  // util.log(output);
-  console.log(output);
+  putStrLn(output);
 }
+
+export function putStrLn(...vs: any[]) {
+  const fmts = _.map(vs, v => {
+    if (_.isString(v)) {
+      return v;
+    }
+    return util.inspect(v, inspectOptionDefaults);
+  });
+  const fmt = fmts.join(" ");
+  process.stdout.write(fmt);
+  process.stdout.write('\n');
+}
+
