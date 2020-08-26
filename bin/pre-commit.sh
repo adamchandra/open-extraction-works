1030ed4c0b5bd5e4ad39ef1c554b9929fb4aed63
Adam Saunders <saunders@cs.umass.edu>
2020-08-26 07:59:13 -0400

    updated pre-commit hook

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -1,4 +1,22 @@
+#!/bin/sh
+
+verbose=0
+
+while getopts "v" name; do
+    case $name in
+        v)    verbose=1;;
+        [?])  showhelp $0;;
+    esac
+done
+
 # Look for TODO/FIX etc.
-numtodo=$(rg '(TODO|FIX|ACS)' workspaces | wc --lines)
+regex='(TODO|FIX|\.only)'
+flags='--type-not html'
+
+# numtodo=$(rg $regex workspaces | wc --lines)
+numtodo=$(rg $flags $regex . | wc --lines)
 echo "There are ~ $numtodo TODOs/FIX/ etc.. in the codebase"
 
+if [ "$verbose" = 1 ]; then
+    rg $flags $regex .
+fi
