48fab8255c91702f1e45a16bba2df4a23563c2dd
Adam Saunders <saunders@cs.umass.edu>
2020-08-31 16:35:35 -0400

    spider timeout/nav bug fixes

--- workspaces/services/src/spidering/scraper.ts
+++ workspaces/services/src/spidering/scraper.ts
@@ -6,6 +6,7 @@ import {
   Response,
   Page,
   Browser,
+  Frame
 } from 'puppeteer';
 
 import puppeteer from 'puppeteer-extra'
@@ -31,7 +32,7 @@ import { createScrapingContext } from './scraping-context';
 export interface Scraper {
   browser: Browser;
   workingDirectory: string;
-  scrapeUrl(url: string): Promise<Metadata|undefined>;
+  scrapeUrl(url: string): Promise<Metadata | undefined>;
   quit(): Promise<void>;
 }
 
@@ -55,12 +56,13 @@ async function scrapeUrl(
   browser: Browser,
   workingDirectory: string,
   url: string
-): Promise<Metadata|undefined> {
+): Promise<Metadata | undefined> {
 
   const scrapingContext = createScrapingContext(workingDirectory, url);
 
   const { rootLogger } = scrapingContext;
   const entryRootPath = getResolvedEntryDownloadPath(scrapingContext);
+  rootLogger.info(`downloading ${url} to ${scrapingContext.entryEncPath.toPath()}`);
 
   const hasMetadata = hasCorpusFile(entryRootPath, '.', 'metadata.json');
 
@@ -73,12 +75,37 @@ async function scrapeUrl(
   try {
     logPageEvents(scrapingContext, page);
 
-    const response: Response | null = await page.goto(url);
+    page.setDefaultNavigationTimeout(11000);
+    page.setDefaultTimeout(11000);
+    page.setJavaScriptEnabled(true);
+
+    let response: Response | null = await page.goto(url, {
+    });
+
+    // const response: Response | null = await page.goto(url);
+
+    if (!response) {
+      const response2 = await page
+        .waitForNavigation({
+          // waitUntil: [ ]
+        })
+        .catch(() => {
+          //
+
+        });
+      if (response2) {
+        response = response2;
+      }
+    }
 
     if (!response) {
       rootLogger.warn(`no response ${url}`);
       return;
     }
+
+    const allFrameContent = await Promise.all(
+      _.map(page.frames(), (frame: Frame) => frame.content())
+    );
 
     const request = response.request();
     const requestHeaders = request.headers();
@@ -89,10 +116,13 @@ async function scrapeUrl(
 
     const respBuffer = await response.buffer();
     writeCorpusTextFile(entryRootPath, '.', 'response-body', respBuffer.toString())
+    _.each(allFrameContent, (frameContent, i) => {
+      writeCorpusTextFile(entryRootPath, '.', `response-frame-${i}`, frameContent);
+    });
 
     const metadata = createMetadata(url, response);
     writeCorpusJsonFile(entryRootPath, '.', 'metadata.json', metadata);
-    const status  = response.status();
+    const status = response.status();
     await page.close();
     rootLogger.info(`Scraped ${url}: status: ${status}`);
     return metadata;
@@ -108,7 +138,10 @@ export async function scrapeUrlAndQuit(
   workingDirectory: string,
   url: string
 ): Promise<void> {
-  const browser: Browser = await puppeteer.launch();
+  const browser: Browser = await puppeteer.launch({
+    // devtools: true,
+    // headless: false
+  });
   await scrapeUrl(browser, workingDirectory, url);
   await browser.close();
 }
